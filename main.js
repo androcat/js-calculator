eee01e54ca36f3a0b999e3277421a55f03d21f4b
Jay Medina <jay.medina.celis@gmail.com>
2023-01-06 11:08:32 -0500

    Define calculate- alert the value of equal sign button

--- main.js
+++ main.js
@@ -8,6 +8,10 @@
     //event is default param
     alert(e.target.value); //document.querySelector("button")
   }
+  function calculate(e) {
+    //event is default param
+    alert(e.target.value); //document.querySelector("button")
+  }
 
   document.querySelectorAll(".number").forEach(function (element) {
     element.addEventListener("click", pushNumber);
@@ -16,4 +20,6 @@
   document.querySelectorAll(".operator, .clear").forEach(function (element) {
     element.addEventListener("click", pushOperator);
   });
+
+  document.querySelector(".equal-sign").addEventListener("click", pushOperator);
 })();
