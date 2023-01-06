9a0e78589200a7518954751c3091a2d51d6e471c
Jay Medina <jay.medina.celis@gmail.com>
2023-01-06 11:05:09 -0500

    Define pushOperator- alert the value of operator buttons

--- main.js
+++ main.js
@@ -4,8 +4,16 @@
     //event is default param
     alert(e.target.value); //document.querySelector("button")
   }
+  function pushOperator(e) {
+    //event is default param
+    alert(e.target.value); //document.querySelector("button")
+  }
 
   document.querySelectorAll(".number").forEach(function (element) {
     element.addEventListener("click", pushNumber);
+  });
+
+  document.querySelectorAll(".operator, .clear").forEach(function (element) {
+    element.addEventListener("click", pushOperator);
   });
 })();
