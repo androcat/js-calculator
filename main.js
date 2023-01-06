992cc606425ebfd0dc79228acc430272a21a1b2d
Jay Medina <jay.medina.celis@gmail.com>
2023-01-06 11:31:59 -0500

    Add values of number and operator buttons to calculation array

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 (function () {
   "use strict";
+
+  let calculation = [];
+
   function pushNumber(e) {
-    //event is default param
-    alert(e.target.value); //document.querySelector("button")
+    alert(e.target.value);
+    calculation.push(parseInt(e.target.value, 10));
   }
   function pushOperator(e) {
-    //event is default param
-    alert(e.target.value); //document.querySelector("button")
+    alert(e.target.value);
+    calculation.push(e.target.value);
   }
   function calculate(e) {
-    //event is default param
-    alert(e.target.value); //document.querySelector("button")
+    alert(e.target.value);
   }
 
   document.querySelectorAll(".number").forEach(function (element) {
     element.addEventListener("click", pushNumber);
   });
-
   document.querySelectorAll(".operator, .clear").forEach(function (element) {
     element.addEventListener("click", pushOperator);
   });
-
   document.querySelector(".equal-sign").addEventListener("click", pushOperator);
+
+  // Keeping track of the calculation array
+  document.querySelectorAll("button").forEach(function (element) {
+    element.addEventListener("click", () => console.log(calculation));
+  });
 })();
