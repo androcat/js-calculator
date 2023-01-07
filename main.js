5206c4487c0c02cdccbc5d6f269c2b58a3dff6ae
Jay Medina <jay.medina.celis@gmail.com>
2023-01-07 14:58:25 -0500

    Alert calulation when = is pressed

--- main.js
+++ main.js
@@ -2,17 +2,50 @@
   "use strict";
 
   let calculation = [];
+  let operator;
+  let num1;
+  let num2;
+  let result;
 
   function pushNumber(e) {
     alert(e.target.value);
-    calculation.push(parseInt(e.target.value, 10));
+    calculation.push(e.target.value); // parseInt(e.target.value, 10));
   }
   function pushOperator(e) {
     alert(e.target.value);
     calculation.push(e.target.value);
   }
   function calculate(e) {
-    alert(e.target.value);
+    alert(calculation.toString().split(",").join(""));
+    console.log(typeof calculation);
+    for (let i = 0; i < calculation.length; i++) {
+      if (
+        calculation[i] === "*" ||
+        calculation[i] === "/" ||
+        calculation[i] === "+" ||
+        calculation[i] === "-"
+      ) {
+        operator = calculation[i];
+        num1 = calculation.slice(0, i).join("");
+        num2 = calculation.slice(i + 1).join("");
+        console.log("num1:", num1);
+        console.log("num2:", num2);
+      }
+    }
+    if (operator === "*") {
+      result = num1 * num2;
+    } else if (operator === "/") {
+      result = num1 / num2;
+    } else if (operator === "+") {
+      result = num1 + num2;
+    } else if (operator === "-") {
+      result = num1 - num2;
+    }
+
+    alert(result);
+
+    calculation = []; //reset calculation for next one
+    console.log("end calculate");
   }
 
   document.querySelectorAll(".number").forEach(function (element) {
@@ -21,7 +54,7 @@
   document.querySelectorAll(".operator, .clear").forEach(function (element) {
     element.addEventListener("click", pushOperator);
   });
-  document.querySelector(".equal-sign").addEventListener("click", pushOperator);
+  document.querySelector(".equal-sign").addEventListener("click", calculate);
 
   // Keeping track of the calculation array
   document.querySelectorAll("button").forEach(function (element) {
